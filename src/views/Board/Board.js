d6fea3735aabca9612ec42aa28bf6b955074fe7c
DaehyeongLee <40898019+DaehyeongLee@users.noreply.github.com>
2021-11-16 22:55:02 +0900

    Edit Handler

--- src/views/Board/Board.js
+++ src/views/Board/Board.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { API_BASE_URL } from '../../config/config';
 import axios from 'axios';
-import * as MC from "@material-ui/core";
-import * as MS from "@material-ui/styles";
+import {Table, TableBody, TableHead, TableRow, TableCell, 
+    ButtonGroup, Button, Card, CardContent, TextField, CircularProgress} from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
 
-const useStyles = MS.makeStyles(theme => ({
+const useStyles = makeStyles(theme => ({
     root: {
 
     },
@@ -20,6 +21,12 @@ function Board(props) {
 
     const [isLoading, setisLoading] = useState(true)
     const [boardList, setboardList] = useState([])
+    const [isEdit, setisEdit] = useState(-1)
+    const [editObject, seteditObject] = useState({
+        title: "",
+        completed: false,
+        created_by: ""
+    })
 
     useEffect(() => {
         const init = () => {
@@ -31,92 +38,182 @@ function Board(props) {
         });
     }, [])
 
+    // 리스트 가져옴
     const getBoardList = () => {
         axios.get(`${API_BASE_URL}/api/task/todo`).then((res) => {
-            console.log(res);
+            setboardList(res.data);
             setisLoading(false)
         }).catch((err) => {
             console.log(err);
         })
     }
 
+    // Input Change Handler
+    const handleChange = (event) => {
+        let name = event.target.name;
+        let value = event.target.value;
+
+        seteditObject(prev => {
+            return {
+                ...prev,
+                [name]: value
+            }
+        })
+    }
+
+    // 수정 버튼 Click Event
+    const clickEdit = (index) => {
+        setisEdit(index)
+        seteditObject(boardList.find(item => item.id === index))
+    }
+
+    // 수정 Submit
+    const handleEdit = () => {
+        axios.put(`${API_BASE_URL}/api/task/todo/update/${isEdit}`, editObject).then((res) => {
+            setisEdit(-1)
+            getBoardList()
+        }).catch((err) => {
+            console.log(err);
+        })
+    }
+
     const tableRow = (obj, index) => (
-        <MC.TableRow
-            hover
-            key={obj.id}>
 
-            {/* ID */}
-            <MC.TableCell align={"center"}>
-                {obj.id}
-            </MC.TableCell>
-            {/* TITLE */}
-            <MC.TableCell align={"left"}>
-                {obj.title}
-            </MC.TableCell>
-            {/* COMPLETED */}
-            <MC.TableCell align={"center"}>
-                {obj.completed}
-            </MC.TableCell>
-            {/* CREATED BY */}
-            <MC.TableCell align={"center"}>
-                {obj.created_by}
-            </MC.TableCell>
+        (obj.id != isEdit) ? (
+            <TableRow
+                hover
+                key={obj.id}>
 
-            {/* BUTTON */}
-            <MC.TableCell align={"center"}>
-                <MC.ButtonGroup
-                    aria-label="text primary button group"
-                >
-                    <MC.Button
-                        style={{
-                            borderTopLeftRadius: 4,
-                            borderBottomLeftRadius: 4
-                        }}
+                {/* ID */}
+                <TableCell align={"center"}>
+                    {obj.id}
+                </TableCell>
+                {/* TITLE */}
+                <TableCell align={"left"}>
+                    {obj.title}
+                </TableCell>
+                {/* COMPLETED */}
+                <TableCell align={"center"}>
+                    {obj.completed.toString()}
+                </TableCell>
+                {/* CREATED BY */}
+                <TableCell align={"center"}>
+                    {obj.created_by}
+                </TableCell>
+
+                {/* BUTTON */}
+                <TableCell align={"center"}>
+                    <ButtonGroup
+                        aria-label="text primary button group"
                     >
-                        수정
-                    </MC.Button>
-                </MC.ButtonGroup>
-            </MC.TableCell>
+                        <Button
+                            style={{
+                                borderTopLeftRadius: 4,
+                                borderBottomLeftRadius: 4
+                            }}
+                            onClick={() => clickEdit(obj.id)}
+                        >
+                            수정
+                        </Button>
+                    </ButtonGroup>
+                </TableCell>
 
-        </MC.TableRow>
+            </TableRow>
+        )
+            :
+            (
+                <TableRow
+                    hover
+                    key={obj.id}>
+
+                    {/* ID */}
+                    <TableCell align={"center"}>
+                        {obj.id}
+                    </TableCell>
+                    {/* TITLE */}
+                    <TableCell align={"left"}>
+                        <TextField
+                            variant="outlined"
+                            onChange={handleChange}
+                            name="title"
+                            value={editObject.title}
+                        />
+                    </TableCell>
+                    {/* COMPLETED */}
+                    <TableCell align={"center"}>
+                        <TextField
+                            variant="outlined"
+                            onChange={handleChange}
+                            name="completed"
+                            value={editObject.completed}
+                        />
+                    </TableCell>
+                    {/* CREATED BY */}
+                    <TableCell align={"center"}>
+                        <TextField
+                            variant="outlined"
+                            onChange={handleChange}
+                            name="created_by"
+                            value={editObject.created_by}
+                        />
+                    </TableCell>
+
+                    {/* BUTTON */}
+                    <TableCell align={"center"}>
+                        <ButtonGroup
+                            aria-label="text primary button group"
+                        >
+                            <Button
+                                style={{
+                                    borderTopLeftRadius: 4,
+                                    borderBottomLeftRadius: 4
+                                }}
+                                onClick={handleEdit}
+                            >
+                                저장
+                            </Button>
+                        </ButtonGroup>
+                    </TableCell>
+                </TableRow>
+            )
     )
 
     return (
-        <MC.Card className={classes.root}>
-            <MC.CardContent className={classes.content}>
-                <MC.Table size="small">
-                    <MC.TableHead>
-                        <MC.TableRow>
-                            <MC.TableCell align={"center"}>ID</MC.TableCell>
-                            <MC.TableCell align={"center"}>TITLE</MC.TableCell>
-                            <MC.TableCell align={"center"}>COMPLETED</MC.TableCell>
-                            <MC.TableCell align={"center"}>CREATED_BY</MC.TableCell>
-                            <MC.TableCell align={"center"}>ACTION</MC.TableCell>
-                        </MC.TableRow>
-                    </MC.TableHead>
-                    <MC.TableBody>
+        <Card className={classes.root}>
+            <CardContent className={classes.content}>
+                <Table size="small">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell align={"center"}>ID</TableCell>
+                            <TableCell align={"center"}>TITLE</TableCell>
+                            <TableCell align={"center"}>COMPLETED</TableCell>
+                            <TableCell align={"center"}>CREATED_BY</TableCell>
+                            <TableCell align={"center"}>ACTION</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
                         {
                             isLoading ?
-                                <MC.TableRow hover>
-                                    <MC.TableCell colSpan={5} align="center">
-                                        <MC.CircularProgress />
-                                    </MC.TableCell>
-                                </MC.TableRow>
+                                <TableRow hover>
+                                    <TableCell colSpan={5} align="center">
+                                        <CircularProgress />
+                                    </TableCell>
+                                </TableRow>
                                 :
                                 boardList.length === 0 ?
-                                    <MC.TableRow>
-                                        <MC.TableCell colSpan={5} align="center">
+                                    <TableRow>
+                                        <TableCell colSpan={5} align="center">
                                             조회된 데이터가 한 건도 없네요.
-                                        </MC.TableCell>
-                                    </MC.TableRow>
+                                        </TableCell>
+                                    </TableRow>
                                     :
                                     boardList.map((item, index) => tableRow(item, index))
                         }
-                    </MC.TableBody>
+                    </TableBody>
 
-                </MC.Table>
-            </MC.CardContent>
-        </MC.Card>
+                </Table>
+            </CardContent>
+        </Card>
     )
 }
 
