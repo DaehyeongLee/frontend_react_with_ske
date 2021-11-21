213f64a2a3f3aebcc22e4180c7f9990fcee964c6
DaehyeongLee <40898019+DaehyeongLee@users.noreply.github.com>
2021-11-21 11:31:19 +0900

    Add, Delete

--- src/views/Board/Board.js
+++ src/views/Board/Board.js
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { API_BASE_URL } from '../../config/config';
 import axios from 'axios';
-import {Table, TableBody, TableHead, TableRow, TableCell, 
-    ButtonGroup, Button, Card, CardContent, TextField, CircularProgress} from "@material-ui/core";
+import {
+    Table, TableBody, TableHead, TableRow, TableCell, Grid,
+    ButtonGroup, Button, Card, CardContent, CardActions, Divider,
+    TextField, CircularProgress
+} from "@material-ui/core";
 import { makeStyles } from "@material-ui/styles";
 
 const useStyles = makeStyles(theme => ({
@@ -21,7 +24,8 @@ function Board(props) {
 
     const [isLoading, setisLoading] = useState(true)
     const [boardList, setboardList] = useState([])
-    const [isEdit, setisEdit] = useState(-1)
+    const [editId, setEditId] = useState(-1)
+    const [isAdd, setisAdd] = useState(false)
     const [editObject, seteditObject] = useState({
         title: "",
         completed: false,
@@ -62,15 +66,60 @@ function Board(props) {
     }
 
     // 수정 버튼 Click Event
-    const clickEdit = (index) => {
-        setisEdit(index)
-        seteditObject(boardList.find(item => item.id === index))
+    const clickEdit = (obj) => {
+        setEditId(obj.id)
+        seteditObject(obj)
     }
 
-    // 수정 Submit
+    // 등록/수정 Submit
     const handleEdit = () => {
-        axios.put(`${API_BASE_URL}/api/task/todo/update/${isEdit}`, editObject).then((res) => {
-            setisEdit(-1)
+        // 등록
+        if (isAdd) {
+            axios.post(`${API_BASE_URL}/api/task/todo/create`, {
+                title: editObject.title,
+                completed: editObject.completed,
+                created_by: editObject.created_by
+            }).then((res) => {
+                setEditId(-1)
+                getBoardList()
+                setisAdd(false)
+            }).catch((err) => {
+                console.log(err);
+            })
+        }
+        // 수정
+        else {
+            axios.put(`${API_BASE_URL}/api/task/todo/update/${editId}`, editObject).then((res) => {
+                setEditId(-1)
+                getBoardList()
+            }).catch((err) => {
+                console.log(err);
+            })
+        }
+    }
+
+    // 등록 버튼 Click Event
+    const clickAdd = () => {
+        setisAdd(true)
+        setEditId(boardList[boardList.length-1].id + 1)
+        setboardList([...boardList, {
+            id: boardList[boardList.length-1].id + 1,
+            title: "",
+            completed: false,
+            created_by: ""
+        }])
+    }
+
+    // 취소 버튼 Click Event
+    const handleCancel = () => {
+        setisAdd(false)
+        setEditId(-1)
+        setboardList(boardList.splice(0, boardList.length - 1))
+    }
+
+    const clickDelete = (id) => {
+        axios.delete(`${API_BASE_URL}/api/task/todo/delete/${id}`).then((res) => {
+            setEditId(-1)
             getBoardList()
         }).catch((err) => {
             console.log(err);
@@ -79,7 +128,7 @@ function Board(props) {
 
     const tableRow = (obj, index) => (
 
-        (obj.id != isEdit) ? (
+        (obj.id != editId) ? (
             <TableRow
                 hover
                 key={obj.id}>
@@ -111,9 +160,18 @@ function Board(props) {
                                 borderTopLeftRadius: 4,
                                 borderBottomLeftRadius: 4
                             }}
-                            onClick={() => clickEdit(obj.id)}
+                            onClick={() => clickEdit(obj)}
                         >
                             수정
+                        </Button>
+                        <Button
+                            style={{
+                                borderTopLeftRadius: 4,
+                                borderBottomLeftRadius: 4
+                            }}
+                            onClick={() => clickDelete(obj.id)}
+                        >
+                            삭제
                         </Button>
                     </ButtonGroup>
                 </TableCell>
@@ -128,7 +186,7 @@ function Board(props) {
 
                     {/* ID */}
                     <TableCell align={"center"}>
-                        {obj.id}
+                        {!isAdd && obj.id}
                     </TableCell>
                     {/* TITLE */}
                     <TableCell align={"left"}>
@@ -172,6 +230,19 @@ function Board(props) {
                             >
                                 저장
                             </Button>
+                            {
+                                isAdd &&
+                                <Button
+                                    style={{
+                                        borderTopLeftRadius: 4,
+                                        borderBottomLeftRadius: 4
+                                    }}
+                                    onClick={handleCancel}
+                                >
+                                    취소
+                                </Button>
+                            }
+
                         </ButtonGroup>
                     </TableCell>
                 </TableRow>
@@ -210,9 +281,23 @@ function Board(props) {
                                     boardList.map((item, index) => tableRow(item, index))
                         }
                     </TableBody>
-
                 </Table>
             </CardContent>
+            <Divider />
+            <CardActions>
+                <Grid container justifyContent={"flex-end"} alignItems={"center"} style={{ padding: "0px 20px" }}>
+                    <ButtonGroup
+                        aria-label="text primary button group"
+                        color="primary">
+                        <Button
+                            onClick={clickAdd}
+                            disabled={isAdd}>
+                            등록
+                        </Button>
+                    </ButtonGroup>
+                </Grid>
+            </CardActions>
+
         </Card>
     )
 }
